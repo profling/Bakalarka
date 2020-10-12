f0675a64f65cb4461fae45cf14e27b7597bcc1f5
ondra Pollák <ondra.pollak@gmail.com>
2020-10-12 19:11:04 +0200

    pripojeni k DB

--- Bakalarka/Bakalarka/Bakalarka/UX/Hierarchie.xaml.cs
+++ Bakalarka/Bakalarka/Bakalarka/UX/Hierarchie.xaml.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySqlConnector;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+using Bakalarka.logika;
 
 namespace Bakalarka.UX
 {
@@ -15,6 +18,17 @@ namespace Bakalarka.UX
         public Hierarchie()
         {
             InitializeComponent();
+            var layout = new StackLayout { Padding = new Thickness(5, 10) };
+            Label label1 = new Label();
+
+            DBConnector databaze = new DBConnector();
+            var prikaz= new MySqlCommand("select;");
+            String test = databaze.ProvedeniPrikazuOstatni(prikaz);
+
+            label1.Text = test;
+            label1.IsVisible = true;
+            layout.Children.Add(label1);
+            this.Content = layout;
         }
     }
 }
---
+++ Bakalarka/Bakalarka/Bakalarka/logika/DBConnector.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using MySqlConnector;
+
+namespace Bakalarka.logika
+{
+    class DBConnector
+    {
+      
+        MySqlConnectionStringBuilder builder;
+        
+       
+
+        public DBConnector()
+        {
+            builder = new MySqlConnectionStringBuilder();
+            builder.Server = "34.107.13.57";
+            builder.Database = "bakalarka";
+            builder.UserID = "app";
+            builder.Password = "lov3MaxiKing";
+            /*builder.SslCert = @"Bakalarka.client-cert.pem";
+            builder.SslKey = @"Bakalarka.client-key.pem";
+            builder.SslCa = @"Bakalarka.server-ca.pem";
+            builder.SslMode = MySqlSslMode.VerifyFull;*/
+        }
+   
+            
+        /**
+         * Provedeni prikazu update, delete, insert. Pokud probehne v poradku vrati data jinak vrati null
+         */
+        
+        public MySqlDataReader ProvedeniPrikazuSelect(MySqlCommand prikaz)
+        {
+            try
+            {
+                
+                MySqlConnection pripojeni = new MySqlConnection(builder.ConnectionString);
+                pripojeni.Open();
+                prikaz.Connection = pripojeni;
+                try
+                {
+                    MySqlDataReader data = prikaz.ExecuteReader();
+                    return data;
+                }
+                catch(MySqlException e)
+                {
+                    return null;
+                }
+
+            }
+            catch (MySqlException e)
+            {
+                return null;
+            }
+        }
+        /**
+         * Provedeni prikazu update, delete, insert. Pokud probehne v poradku vrati null jinak vrati chybovou hlasku
+         */
+        public String ProvedeniPrikazuOstatni( MySqlCommand prikaz)
+        {
+            try
+            {
+                MySqlConnection pripojeni = new MySqlConnection(builder.ConnectionString);
+                pripojeni.Open();
+                prikaz.Connection=pripojeni;
+                try
+                {
+                    prikaz.ExecuteNonQuery();
+                    pripojeni.Close();
+                    return null;
+                }
+                catch (MySqlException e)
+                {
+                    return e.ToString();
+                }
+
+            }
+            catch (MySqlException e)
+            {
+                return e.ToString();
+            }
+
+        }
+        
+    }
+}
